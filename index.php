9122815d8f02f99e18567aecab6082e06096aef1
sucun21 <suk3c2uit@gmail.com>
2015-01-12 09:24:33 +0700

    12012015

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content=""/>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+        
+            <div id="logo">
+                <h3><a href="#">
+                <span class="1">THE</span><br/>
+                <span class="2"> NEWS REPORTER</span></a></h3>
+            </div><!--end#logo-->
+            
+            <div id="pages">
+            
+                <ul class="page">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">Subcribe</a>
+                    </li>
+                    
+                     <li class="last">
+                        <a href="#">Login</a>
+                    </li>
+                </ul>
+                
+            </div><!--end#pages-->
+            
+            <div id="searchBox">
+            
+                <form class="searchForm" action="" method="get">
+                    <p><input type="text" id="search" value ="--- Search this site ---"/></p>
+                </form><!--end.searchForm-->
+                
+            </div><!--end#searchBox-->
+            
+            <div id="sub">
+            </div><!--end#sub-->
+            
+            <div id="naves">
+                <ul class="nav">
+                    <li>
+                        <a href="#">WORLD NEWS</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">SPORTS</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">TECH</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">BUSSINESS</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">ENTERTAINMENT</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">CULTURE</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">BOOK</a>
+                    </li>
+                    
+                     <li>
+                        <a href="#">CLASSCIFIEDS</a>
+                    </li>
+                     <li>
+                        <a href="#">BLOGS</a>
+                    </li>
+                </ul>
+            </div><!--end#naves-->
+            
+        </div><!-- end#header-->
+        
+        <div id="primary">
+            
+            <div id="content">
+                <div id="feature">
+                    <div class="featMain">
+                        <img/>
+                        <div>
+                             <p>aaa aaa aaaaaaaa aaaa aaaaaaa aaa aaaaaaa</p>
+                        </div>  
+                    </div> <!--end .featMain-->
+					<div id=featslides>
+						<div class="featslide">
+						   <img />
+						</div>
+						<div class="featslide">
+						   <img />
+						</div>
+						<div class="featslide">
+						   <img />
+						</div>
+						<div class="featslide">
+						   <img />
+						</div>
+					</div> <!--end #featslides-->
+                    <p class="break">BREAKING NEWS</p>
+                </div><!--end #feature-->
+				<div class="category">
+					<p class='first'> MORE + </p>
+					<h3>AROUND THE WORLD</h3>
+					<div class="postCate">
+						<a href=""><img/>the world is big.a a a aaaaaa aa a a a a a</a>
+						<p> Read More + </p>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/>the world is big.a a a aaaaaa aa a a a a a</a>
+						<p> Read More + </p>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/>the world is big.a a a aaaaaa aa a a a a a</a>
+						<p> Read More + </p>
+					</div><!--end .postCate-->
+					
+				</div><!--end .category-->
+				<div class="category">
+					<p class='first'> MORE + </p>
+					<h3>LASTEST ARTICLES</h3>
+					<div class="postCate">
+						<a href=""><img/>the world is big.a a a aaaaaa aa a a a a a</a>
+						<p> Read More + </p>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/>the world is big.a a a aaaaaa aa a a a a a</a>
+						<p> Read More + </p>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/>the world is big.a a a aaaaaa aa a a a a a</a>
+						<p> Read More + </p>
+					</div><!--end .postCate-->
+					
+				</div><!--end .category-->
+				<div class="category">
+					<p class='first'> MORE + </p>
+					<h3>BALLERY</h3>
+					<div class="postCate">
+						<a href=""><img/></a>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/></a>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/></a>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/></a>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/></a>
+					</div><!--end .postCate-->
+					<div class="postCate">
+						<a href=""><img/></a>
+					</div><!--end .postCate-->
+					
+				</div><!--end .category-->
+				<div class="category">
+					<p class='first'> MORE + </p>
+					<h3>TECH NEWS</h3>
+					<div class="LastpostCate">
+						<a href=""> a b v b b b b b b b b b b b b b b b</a>
+					</div><!--end .LastpostCate-->
+					<div class="LastpostCate">
+						<a href="">b d d d d d d d d d d d d d d d d d d</a>
+					</div><!--end .LastpostCate-->
+					<div class="LastpostCate">
+						<a href="">b d d d d d d d d d d d d d d d d d d</a>
+					</div><!--end .LastpostCate-->
+					<div class="LastpostCate">
+						<a href="">b d d d d d d d d d d d d d d d d d d</a>
+					</div><!--end .LastpostCate-->
+					
+				</div><!--end .category-->
+                
+            </div><!--end #content-->
+            
+            <div id="sideBar">
+                <div id="randPosts">
+                    <div class="randpost">
+                    	<a href="#"><img src=""/></a>
+                    </div><!--end .randpost-->
+					<div class="randpost">
+                    	<a href="#"><img src=""/></a>
+                    </div><!--end .randpost-->
+					<div class="randpost">
+                    	<a href="#"><img src=""/></a>
+                    </div><!--end .randpost-->
+					<div class="randpost">
+                    	<a href="#"><img src=""/></a>
+                    </div><!--end .randpost-->
+					<a href=""><p>MORE + </p></a>
+                </div><!--end .randPost-->
+            </div><!--end#sideBar-->
+            
+        </div><!--end#primary-->
+        
+        <div id="footer">
+        
+            <div id="navFooter">
+            </div><!--end#navFooter-->
+            
+            <div id="contFooter">
+            </div><!--end#contFooter-->
+        </div><!--end#footer-->
+    </div><!--end #wrapper-->
+</body>
+</html>
